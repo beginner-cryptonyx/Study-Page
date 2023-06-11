6a48a5ddf226b0a933b04f036125a1b544a4cf7b
Crypto <mayanksethi242008@gmail.com>
2023-06-11 14:09:42 +0530

    Added a color pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'background':'#1d1b1c',
+                'text':'#fbf4f8',
+                'primary':'#9ec671',
+                'secondary':'#000000',
+                'accent#1':'#719ec6',
+                'accent#2':'#61aeab',
+                'accent#3':'#74b672',
+              }
+        },
     },
     plugins: [],
 };
