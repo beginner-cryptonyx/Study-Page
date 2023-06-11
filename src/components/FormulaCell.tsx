7aab986775f5bb7ffd738d97ccce7e2ccc2ec74d
Crypto <mayanksethi242008@gmail.com>
2023-06-11 14:20:37 +0530

    Added importance prop

--- src/components/FormulaCell.tsx
+++ src/components/FormulaCell.tsx
@@ -1,15 +1,14 @@
 interface Props {
     formula: string;
     description: string;
-    f_width?: any;
-    d_width?: any;
+    importance?: string;
 }
 
 function Cell(props: Props) {
     return (
-        <div className="flex flex-row w-[70vw] mx-auto">
-            <div className="w-1/2">{props.formula}</div>
-            <div className="w-1/2">{props.description}</div>
+        <div className="flex flex-row w-[70vw] mx-auto text-center">
+            <div className="w-1/2 bg-gradient-to-r">{props.formula}</div>
+            <div className="w-1/2 bg-gradient-to-l">{props.description}</div>
         </div>
     );
 }
