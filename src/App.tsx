f36f1253352c1cc6183335e1a0fc7cbeafa5dae4
Crypto <mayanksethi242008@gmail.com>
2023-06-14 20:44:38 +0530

    Re-added the route code and commented the cell testing code

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import "./App.css";
 import Cell from "./components/FormulaCell";
 import { Route, Routes } from "react-router-dom";
+import Card from "./components/Card";
 
 function App() {
+    
     return (
         <>
-            <Cell formula="XD" description="DX"/>
+            <Card header="Test" content="Lmao" tags={["hi", "hello"]}/>
+            {/* <Cell formula="XD" description="DX"/> */}
             <Routes>
                 <Route path="/"></Route>
-                {/* <Route path="/Textbooks"></Route>
+                <Route path="/Textbooks"></Route>
                 <Route path="/formulas">
                     <Route path=":id"></Route>
-                </Route> */}
+                </Route>
             </Routes>
         </>
     );
