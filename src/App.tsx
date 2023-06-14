14774b8690d91e4ac73ace21ac87cbda6ad3f1a6
Crypto <mayanksethi242008@gmail.com>
2023-06-14 22:38:56 +0530

    Implemented typewriter effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 import "./App.css";
 import Cell from "./components/FormulaCell";
+import Typewriter from "typewriter-effect";
 import { Route, Routes } from "react-router-dom";
 import Card from "./components/Card";
 
 function App() {
-    
     return (
         <>
-            <Card header="Test" content="Lmao" tags={["hi", "hello"]}/>
+            <div className="text-[40pt] flex justify-center text-green-500">
+                <Typewriter
+                    onInit={(tw) => {
+                        tw.typeString(
+                            "Find Literally Everything Here"
+                        );
+                        tw.pauseFor(900)
+                        tw.deleteChars(25);
+                        tw.typeString("The Textbooks You Need")
+                        tw.pauseFor(700)
+                        tw.deleteAll()
+                        tw.typeString("Find All The Formulae")
+                        tw.deleteChars(16)
+                        tw.typeString("Resources")
+                        tw.pauseFor(500)
+                        tw.typeString(" & Our Syllabus")
+                        tw.start()
+                    }}
+                    options={{
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+            </div>
+            <Card header="Test" content="Lmao" tags={["hi", "hello"]} />
             {/* <Cell formula="XD" description="DX"/> */}
             <Routes>
                 <Route path="/"></Route>
