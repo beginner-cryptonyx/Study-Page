165c529b885c95f413fc7e8d3f242c9bb58e3631
Crypto <mayanksethi242008@gmail.com>
2023-06-15 20:15:20 +0530

    Added snazzy animations

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,23 @@ import Card from "./components/Card";
 function App() {
     return (
         <>
-            <div className="text-[40pt] flex justify-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 animate-text_side">
+            <div className="text-[40pt] flex justify-center text-green-500">
                 <Typewriter
                     onInit={(tw) => {
-                        tw.typeString("Find Literally Everything Here");
-                        tw.pauseFor(900);
+                        tw.typeString(
+                            "Find Literally Everything Here"
+                        );
+                        tw.pauseFor(900)
                         tw.deleteChars(25);
-                        tw.typeString("The Textbooks You Need");
-                        tw.pauseFor(700);
-                        tw.deleteAll();
-                        tw.typeString("Find All The Formulae");
-                        tw.deleteChars(16);
-                        tw.typeString("Resources");
-                        tw.pauseFor(500);
-                        tw.typeString(" & Our Syllabus");
-                        tw.start();
+                        tw.typeString("The Textbooks You Need")
+                        tw.pauseFor(700)
+                        tw.deleteAll()
+                        tw.typeString("Find All The Formulae")
+                        tw.deleteChars(16)
+                        tw.typeString("Resources")
+                        tw.pauseFor(500)
+                        tw.typeString(" & Our Syllabus")
+                        tw.start()
                     }}
                     options={{
                         autoStart: true,
@@ -30,24 +32,8 @@ function App() {
                     }}
                 />
             </div>
-
-            <div className="grid grid-cols-3 mx-auto w-[90vw] mt-6">
-                <Card
-                    header="Physics Formulae"
-                    content="Lmao"
-                    tags={["hi", "hello"]}
-                />
-                <Card
-                    header="Math Formulae"
-                    content="Lmao"
-                    tags={["hi", "hello"]}
-                />
-                <Card
-                    header="Chemistry Formulae"
-                    content="Lmao"
-                    tags={["hi", "hello"]}
-                />
-            </div>
+            <Card header="Test" content="Lmao" tags={["hi", "hello"]} />
+            {/* <Cell formula="XD" description="DX"/> */}
             <Routes>
                 <Route path="/"></Route>
                 <Route path="/Textbooks"></Route>
