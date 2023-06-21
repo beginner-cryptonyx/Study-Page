c58b5bca90fbadcf7f7b928148e69dacf0d01ee5
Crypto <mayanksethi242008@gmail.com>
2023-06-21 22:36:07 +0530

    Renamed file and re-envisioned the Cells and their structure

---
+++ src/Pages/FormulaeDisplayerMath.tsx
@@ -0,0 +1,72 @@
+import FormulaCell from "../components/FormulaCell";
+
+function FormulaeMath() {
+    return (
+        <>
+            <div className="mt-7">
+                <FormulaCell
+                    formula="yo"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+                <FormulaCell
+                    formula="E=mc2"
+                    description="Theory of Relativity"
+                    importance="normal"
+                />
+            </div>
+        </>
+    );
+}
+
+export default FormulaeMath;
