121fa6d4c5e3dc5f29bde0d570618418f137f0ef
Crypto <mayanksethi242008@gmail.com>
2023-06-29 20:56:02 +0530

    Added Separator Cell

--- src/components/FormulaCell.tsx
+++ src/components/FormulaCell.tsx
@@ -1,34 +1,27 @@
 import "../App.css";
-import { useState } from "react";
 import { MathJaxContext, MathJax } from "better-react-mathjax";
 const Mathml2latex = require("mathml-to-latex");
-interface MathCellProps {
-    formula: any;
+export interface MathCellProps {
+    formula: string;
     description: string;
     importance?: string;
 }
 
+export interface CellSeparatorProps {
+    title: string;
+}
+
+const mathJaxConfig = {
+    tex: {
+        inlineMath: [["$", "$"]],
+    },
+};
+
 function MathCell({ formula, description, importance }: MathCellProps) {
-    const [color, setColor] = useState(
-        importance === "normal"
-            ? "normal"
-            : importance === "important"
-            ? "fs_red"
-            : "fs_grey"
-    );
-
-    const mathJaxConfig = {
-        tex: {
-            inlineMath: [["$", "$"]],
-        },
-    };
-
     const latex_formula = Mathml2latex.convert(formula);
 
     return (
-        <div
-            className={`flex flex-row w-[70vw] mx-auto text-center formula-cell-2 duration-500 ${importance}`}
-        >
+        <div className={`formula-cell-2 duration-500 ${importance ? importance : 'normal'}`}>
             <div
                 className={`w-1/2 bg-gradient-to-r border border-gray-800 border-solid p-2  border-l-2 border-l-black duration-500`}
             >
@@ -45,4 +38,13 @@ function MathCell({ formula, description, importance }: MathCellProps) {
     );
 }
 
+function CellSeparator({ title }: CellSeparatorProps) {
+    return (
+        <div className="bg-slate-500 w-[70vw] mx-auto text-center font-bold mb-[0.0625rem] p-3 border-black border-1 border-x-2 mt-8 text-3xl">
+            {title}
+        </div>
+    );
+}
+
 export default MathCell;
+export { MathCell, CellSeparator };
