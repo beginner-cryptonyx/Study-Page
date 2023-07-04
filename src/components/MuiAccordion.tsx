72692300dcbb7ead410a1ad40a6500aee395ed25
Crypto <mayanksethi242008@gmail.com>
2023-07-04 20:22:25 +0530

    Styled the accordion

--- src/components/MuiAccordion.tsx
+++ src/components/MuiAccordion.tsx
@@ -2,25 +2,38 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material";
 // import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#505050",
+        },
+    },
+});
 export interface MUIaccordionProps {
-    content: Array<string>;
+    content: Array<any>;
 }
 
 function MUIaccordion({ content }: MUIaccordionProps) {
     return (
         <>
-            
-                <Accordion>
-                    <AccordionSummary>
-                        <Typography>{content[0]}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        {content[1]}
-                    </AccordionDetails>
-                </Accordion>
-            
+            <ThemeProvider theme={theme}>
+                <div className="w-[70vw] mx-auto">
+                    <Accordion
+                        sx={{
+                            bgcolor: "primary.main",
+                            textAlign: 'center'
+                        }}
+                    >
+                        <AccordionSummary>
+                            <Typography>{content[0]}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>{content[1]}</AccordionDetails>
+                    </Accordion>
+                </div>
+            </ThemeProvider>
         </>
     );
 }
