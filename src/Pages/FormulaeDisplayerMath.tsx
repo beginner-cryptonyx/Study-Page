57f4940bb88750dda533a90d81ec3c3929b1e00c
Crypto <mayanksethi242008@gmail.com>
2023-07-09 16:21:40 +0530

    Finished section 1 of the math formulae

--- src/Pages/FormulaeDisplayerMath.tsx
+++ src/Pages/FormulaeDisplayerMath.tsx
@@ -4,12 +4,33 @@ import MUIaccordion from "../components/MuiAccordion";
 const AlgebraicMathFormulae: Array<Array<any>> = [
     [`\(a + b\)^2 = a^2 + b^2 +2ab`, "Algebric Indentity #1", "normal"],
     [`\(a - b\)^2 = a^2 + b^2 -2ab`, "Algebraic Identity #2 ", "normal"],
-    [`x^n × x^m = x^{m + n}`, "Algebraic Identity #3", "normal"],
-    [`a^2 - b^2 = \(a + b\)×\(a - b\)`, "Algebraic Identity #3", "normal"],
-    [`a^2 - b^2 = \(a + b\)×\(a - b\)`, "Algebraic Identity #3", "normal"],
-    [`a^2 - b^2 = \(a + b\)×\(a - b\)`, "Algebraic Identity #3", "normal"],
-    [`a^2 - b^2 = \(a + b\)×\(a - b\)`, "Algebraic Identity #3", "normal"],
-    [`a^2 - b^2 = \(a + b\)×\(a - b\)`, "Algebraic Identity #3", "normal"],
+    [`a^2 - b^2 = (a + b) \\cdot (a - b)`, "Algebraic Identity #3 ", "normal"],
+    [
+        `x^n \\cdot x^m = x^{m + n}`,
+        "Multiplying The Same Number With Indices",
+        "normal",
+    ],
+    [
+        `\\frac{x^m}{x^n} = x^{m-n}`,
+        "Dividing The Same Number With Indices",
+        "normal",
+    ],
+    [
+        `(x^n)^m = x^{n \\cdot m}`,
+        "Adding An Exponent To A Number Already Having One",
+        "normal",
+    ],
+    [
+        `(\\frac{y}{x})^n = \\frac{y^n}{x^n}`,
+        "Adding An Exponent To A Fraction",
+        "normal",
+    ],
+    [`x^{-m} = \\frac{1}{x^m}`, "Number With Negative Exponent", "normal"],
+    [
+        `x^{\\frac{m}{n}} = \\sqrt[n]{x^m}`,
+        "Number With A Fractional Exponent",
+        "normal",
+    ],
 ];
 
 function FormulaeMath() {
