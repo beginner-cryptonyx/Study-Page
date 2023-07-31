eec17b3708cd046b3cacffe7e1faccfa5614fd82
Crypto <mayanksethi242008@gmail.com>
2023-07-31 20:50:02 +0530

    Fixed routing to the chemistry page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import FormulaeMath from "./Pages/FormulaeDisplayerMath";
+import FormulaeChemistry from "./Pages/FormulaeDisplayerChemistry";
 import Home from "./Pages/Home";
 
 function App() {
@@ -11,9 +12,12 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/Textbooks"></Route>
             <Route path="/formulae">
-                <Route path="/formulae/math"  element={<FormulaeMath/>}></Route>
+                <Route path="/formulae/math" element={<FormulaeMath />}></Route>
                 <Route path="/formulae/physics"></Route>
-                <Route path="/formulae/chemistry"></Route>
+                <Route
+                    path="/formulae/chemistry"
+                    element={<FormulaeChemistry />}
+                ></Route>
             </Route>
         </Routes>
     );
