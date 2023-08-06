024eb0b04902eab4cf3c8197e0f064e93d3f4a0c
Crypto <mayanksethi242008@gmail.com>
2023-08-06 17:02:29 +0530

    Added new formulae

--- src/Pages/FormulaeDisplayerMath.tsx
+++ src/Pages/FormulaeDisplayerMath.tsx
@@ -1,6 +1,7 @@
 import { MathCell, CellSeparator } from "../components/FormulaCell";
 import MUIaccordion from "../components/MuiAccordion";
 import { MathJaxContext } from "better-react-mathjax";
+import Formulae from "./Displayer";
 
 const mathJaxConfig = {
     loader: { load: ["[tex]/html"] },
@@ -15,6 +16,7 @@ const mathJaxConfig = {
 };
 
 const AlgebraicMathFormulae: Array<Array<any>> = [
+    [`separator`, "Algebraic Identities"],
     [`(a + b)^2 = a^2 + b^2 +2ab`, "Algebric Indentity #1", "normal"],
     [`(a - b)^2 = a^2 + b^2 -2ab`, "Algebraic Identity #2 ", "normal"],
     [`a^2 - b^2 = (a + b) \\cdot (a - b)`, "Algebraic Identity #3 ", "normal"],
@@ -44,6 +46,20 @@ const AlgebraicMathFormulae: Array<Array<any>> = [
         "Number With A Fractional Exponent",
         "normal",
     ],
+    ["separator", "Sequences"],
+    [
+        `a_n = a_1 + (n - 1)\\cdot d`,
+        "formula for finding nth term of an arithmetic sequence",
+        "normal",
+    ],
+    [
+        `a_n = ar^{n-1}`,
+        "formula for finding nth term of an geometric progression (eg: 1,3,9,27)",
+        "external",
+    ],
+    [``, "", "normal"],
+    [``, "", "normal"],
+    [``, "", "normal"],
 ];
 
 const GeometricalMathFormulae: Array<Array<any>> = [
@@ -53,17 +69,17 @@ const GeometricalMathFormulae: Array<Array<any>> = [
     [
         `C = \\pi d \\text{ or } 2\\pi r`,
         "Used to calculate perimeter of circle",
-        "normal",
+        "important",
     ],
     [`A = s^2`, "Used to calculate area of square", "normal"],
-    [`A = L\\cdot B`, "Used to calculate area of rectangle", "normal"],
+    [`A = L ⋅ B`, "Used to calculate area of rectangle", "normal", 'false'],
     [
         `A = \\frac{1}{2} \\cdot bh`,
         "Used to calculate area of triangle",
         "normal",
     ],
-    [`A = \\pi r^2`, "Used to calculate area of circle", "normal"],
-    [`V = L\\cdot B\\cdot H`, "Used to calculate volume of cuboid", "normal"],
+    [`A = \\pi r^2`, "Used to calculate area of circle", "important"],
+    [`V = L⋅B⋅H`, "Used to calculate volume of cuboid", "normal", 'false'],
     [`V = s^3`, "Used to calculate volume of cube", "normal"],
     [`V = \\pi r^2h`, "Used to calculate volume of cylinder", "normal"],
     [
@@ -79,27 +95,27 @@ const GeometricalMathFormulae: Array<Array<any>> = [
     [
         `C = \\frac{\\theta}{360}\\cdot \\pi r^2`,
         "Used to calculate area of a sector",
-        "external",
+        "normal",
     ],
     [
         `C = \\frac{\\theta}{360}\\cdot 2\\pi r`,
         "Used to calculate arc length",
-        "external",
+        "normal",
     ],
     [
         `V = \\frac{1}{3} \\cdot \\pi r^2h`,
         "Used to calculate volume of cone",
-        "normal",
+        "external",
     ],
     [
         `\\text{SA} = 4\\pi r^2`,
         "Used to calculate surface area of sphere",
-        "normal",
+        "external",
     ],
     [
         `V = \\frac{4}{3} \\pi r^3`,
         "Used to calculate volume of sphere",
-        "extra",
+        "external",
     ],
     [
         `a^2 + b^2 = c^2`,
@@ -137,23 +153,25 @@ const GeometricalMathFormulae: Array<Array<any>> = [
         "Trigonometric formula which gives a product of 1",
         "normal",
     ],
-    [`\\sin{θ} = \\sin{180 - θ}`, "Alternative sine formula", "important"],
-    [`cos(θ) = -cos(180 - θ)`, "Alternative cosine formula", "important"],
+    [`\\sin{θ} = \\sin{180 - θ}`, "Alternative sine formula", "normal"],
+    [`cos(θ) = -cos(180 - θ)`, "Alternative cosine formula", "normal"],
     [
         `cos(θ) = cos(360-θ)`,
         "Alternative cosine formula which has positive value",
-        "important",
+        "normal",
     ],
-    [`tan(θ) = tan(180 + θ)`, "Alternative Tangent formula", "important"],
+    [`tan(θ) = tan(180 + θ)`, "Alternative Tangent formula", "normal"],
     [
         `\\frac{a}{\\sin(A)} = \\frac{b}{\\sin(B)} = \\frac{c}{\\sin(C)}
     `,
         "Used to calculate sides and angles of a triangle",
-        "important",
+        "normal",
     ],
+
+    [`c^2 = a^2 + b^2 -2ab\\cos{C}`, 'Cosine rule', "important"],
     [
-        `\\text{main side} = \(\\text{other sides}\)^2 \\
-         - 2\(\\text{other sides}\) \\times \\cos\(\\text{main angle}\)`,
+        `\\text{main side}^2 = (\\text{other sides}\)^2 \\
+         - 2(\\text{other sides}) \\times \\cos(\\text{main angle})`,
         "simplifies cosine ratios, main side is the side you want to use, main angle is the angle opposite to the side you want to find, if you learn to use this there is no need to remember the above 3 formulas",
         "external",
     ],
@@ -161,11 +179,6 @@ const GeometricalMathFormulae: Array<Array<any>> = [
         `A = \\frac{1}{2} \\cdot a \\cdot b \\cdot \\sin{C}`,
         "Using to calculate area of triangle when height is not given",
         "important",
-    ],
-    [
-        `\\text{Area} = \\frac{1}{2} \\cdot \\text{given sides} \\cdot \\text{sin(given angle)}`,
-        "Using to calculate area of triangle when height is not given",
-        "normal",
     ],
     [
         `Area = \\frac{1}{2} \\cdot \\text{(given sides)} \\cdot \\text{sin(given angle)}`,
@@ -322,7 +335,26 @@ const StatisticalMathFormulae: Array<Array<any>> = [
         "Used to find the position of a percentile",
         "normal",
     ],
+    [`separator`, "Probability"],
+    [
+        `P(A) = (number of favorable outcomes)/(number of possible outcomes)`,
+        "probability of A occurring",
+        "normal",
+        "false",
+    ],
+    [` P(Ā) = 1 - P(A)`, "probability of A not occurring", "normal"],
+    [
+        `P(\\text{A & B}) = P(A) \\cdot P(B)`,
+        "Probability of A and B Occurring",
+        "normal",
+    ],
+    [
+        `P(\\text{A or B}) = P(A) + P(B)`,
+        "Probability of A or B Occurring",
+        "normal",
+    ],
     [``, "", "normal"],
+
 ];
 
 function FormulaeMath() {
