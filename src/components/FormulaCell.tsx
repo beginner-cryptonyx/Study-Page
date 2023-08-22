344277c5e5ba40af6073d187583956f37c5c48f1
Crypto <mayanksethi242008@gmail.com>
2023-08-22 20:37:22 +0530

    More responsive changes

--- src/components/FormulaCell.tsx
+++ src/components/FormulaCell.tsx
@@ -31,27 +31,27 @@ function MultiCell({
     return columns === 3 ? (
         <div className={`formula-cell-2 ${importance ? importance : "normal"}`}>
             <div
-                className={`formulae-3 target w-1/3 bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black flex duration-500 justify-center align-middle items-center ${
+                className={`state0 formulae-3 target w-[100%] md:w-1/3 bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black flex duration-500 justify-center align-middle items-center ${
                     jax ? "_jax" : ""
                 }`}
             >
-                <div className="mx-auto text-3xl h-fit">
+                <div className="mx-auto h-fit">
                     {jax ? <MathJax>{`$${formula}$`}</MathJax> : formula}
                 </div>
             </div>
 
             <div
-                className={`target w-1/3 bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap  duration-500  justify-center align-bottom`}
+                className={`state1 hidden md:target md:w-1/3 bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap  duration-500  justify-center align-bottom`}
             >
-                <div className="desc text-center h-[100%] w-[100%]  p-2 text-3xl">
+                <div className="desc text-center h-[100%] w-[100%]  p-2">
                     {description}
                 </div>
             </div>
 
             <div
-                className={`third target w-1/3 bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap duration-500 justify-center align-bottom`}
+                className={`state2 hidden md:target md:w-1/3 bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap duration-500 justify-center align-bottom`}
             >
-                <div className="flex justify-center items-center text-center h-[100%] w-[100%] p-2 text-3xl">
+                <div className="">
                     {third_content}
                 </div>
             </div>
@@ -59,18 +59,18 @@ function MultiCell({
     ) : columns === 2 ? (
         <div className={`formula-cell-2 ${importance ? importance : "normal"}`}>
             <div
-                className={`formula target w-[100%] md:w-1/2 bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black flex duration-500 justify-center align-middle items-center ${
+                className={`state0 formula target w-[100%] md:w-1/2 bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black duration-500 justify-center align-middle items-center ${
                     jax ? "_jax" : ""
                 }`}
             >
-                <div className="mx-auto text-3xl h-fit">
+                <div className="mx-auto h-fit">
                     {jax ? <MathJax>{`$${formula}$`}</MathJax> : formula}
                 </div>
             </div>
             <div
-                className={`target flex bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap justify-center duration-500 align-middle items-center`}
+                className={`state1 hidden md:target md:w-1/2 bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap justify-center duration-500 align-middle items-center`}
             >
-                <div className="flex justify-center items-center text-center h-[100%] w-[100%] text-3xl p-2">
+                <div className="desc">
                     {description}
                 </div>
             </div>
@@ -78,18 +78,18 @@ function MultiCell({
     ) : description ? (
         <div className={`formula-cell-2 ${importance ? importance : "normal"}`}>
             <div
-                className={`target w-[100%] md:w-1/2 bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black flex duration-500 justify-center align-middle items-center ${
+                className={`state0 target w-[100%] md:w-1/2 bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black duration-500 items-center ${
                     jax ? "_jax" : ""
                 }`}
             >
-                <div className="mx-auto text-3xl h-fit">
+                <div className="mx-auto h-fit">
                     {jax ? <MathJax>{`$${formula}$`}</MathJax> : formula}
                 </div>
             </div>
             <div
-                className={`target hidden md:w-1/2 md:flex bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap justify-center duration-500 align-bottom`}
+                className={`state1 hidden md:target w-1/2  bg-gradient-to-l border border-gray-900 p-2 border-r-2 border-r-black flex-wrap duration-500`}
             >
-                <div className="flex justify-center items-center text-center h-[100%] w-[100%] p-2">
+                <div className="">
                     {description}
                 </div>
             </div>
@@ -97,11 +97,11 @@ function MultiCell({
     ) : (
         <div className={`formula-cell-2 ${importance ? importance : "normal"}`}>
             <div
-                className={`target w-[100%] bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black flex duration-500 justify-center align-middle items-center ${
+                className={`state0 target w-[100%] bg-gradient-to-r border border-gray-800 border-solid border-l-2 border-l-black duration-500 items-center ${
                     jax ? "_jax" : ""
                 }`}
             >
-                <div className="mx-auto text-3xl h-fit">
+                <div className="mx-auto text-2xl md:text-3xl h-fit">
                     {jax ? <MathJax>{`$${formula}$`}</MathJax> : formula}
                 </div>
             </div>
@@ -145,16 +145,58 @@ function MathCell({ formula, description, importance, jax }: MathCellProps) {
 }
 
 function CellSeparator({ title }: CellSeparatorProps) {
-    
-        return (
-            <div
-                className={`bg-slate-500 mx-auto text-center flex font-bold mb-[0.0625rem] p-3 border-black border-2 border-b-0 mt-8 text-3xl`}
-            >
-                <button>{"<"}</button>
-                {title}
-                <button>{">"}</button>
-            </div>
+    const [displayValue, setDisplayValue] = useState(0);
+
+    const screenWidth = window.innerWidth;
+    const stateSelectors = [".state0", ".state1", ".state2"];
+
+    function HandleStateChange(fromState: number, toState: number) {
+        const fromIndex = Array.from(
+            document.querySelectorAll(stateSelectors[fromState])
         );
+
+        const toIndex = Array.from(
+            document.querySelectorAll(stateSelectors[toState])
+        );
+
+        fromIndex.forEach((element) => element.classList.add("hidden"));
+        fromIndex.forEach((element) => element.classList.remove("target"));
+        toIndex.forEach((element) =>
+            element.classList.remove("w-1/2", "w-1/3")
+        );
+        toIndex.forEach((element) => element.classList.add("w-[100%]"));
+        toIndex.forEach((element) => element.classList.add("target"));
+    }
+
+    function Forward() {
+        if (displayValue === 0) {
+            HandleStateChange(0, 1);
+            setDisplayValue(1);
+        } else if (displayValue === 1) {
+            HandleStateChange(1, 2);
+            setDisplayValue(2);
+        }
+    }
+
+    function Reverse() {
+        if (displayValue === 2) {
+            HandleStateChange(2, 1);
+            setDisplayValue(1);
+        } else if (displayValue === 1) {
+            HandleStateChange(1, 0);
+            setDisplayValue(0);
+        }
+    }
+
+    return (
+        <div
+            className={`bg-slate-500 mx-auto text-center flex justify-center font-bold mb-[0.0625rem] p-3 border-black border-2 border-b-0 mt-8 text-2xl md:text-3xl`}
+        >
+            <button onClick={Reverse}>{"<"}</button>
+            {title}
+            <button onClick={Forward}>{">"}</button>
+        </div>
+    );
 }
 
 export default MathCell;
