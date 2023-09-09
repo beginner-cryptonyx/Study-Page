daa6c82215b8cfbb29e5f962a2f88401fdf07daa
Crypto <mayanksethi242008@gmail.com>
2023-09-09 14:15:14 +0530

    Made the Accordion content look more interesting

--- src/components/MuiAccordion.tsx
+++ src/components/MuiAccordion.tsx
@@ -29,7 +29,7 @@ function MUIaccordion({ content }: MUIaccordionProps) {
                         sx={{
                             bgcolor: "primary.main",
                             mx: "auto",
-                            my: "0.1rem"
+                            my: "0.1rem",
                         }}
                     >
                         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
@@ -38,14 +38,29 @@ function MUIaccordion({ content }: MUIaccordionProps) {
                                     mx: "auto",
                                     color: "primary.light",
                                     fontSize: "22px",
-                                    fontWeight: 'bold'
+                                    fontWeight: "bold",
                                 }}
                             >
                                 {content[0]}
                             </Typography>
                         </AccordionSummary>
-                        <AccordionDetails sx={{ bgcolor: "primary.dark" }} className="AccordionDetails">
-                            {content[1]}
+                        <AccordionDetails
+                            style={{
+                                backgroundImage:
+                                    "linear-gradient(to right, #A200FF, #FF00DD, #A200FF)",
+                                animation:
+                                    "gradient_slide_left 3s ease infinite",
+                                filter: "saturate(2%);",
+                                // backdropFilter: "opacity(10%)"
+                            }}
+                            // sx={{ bgcolor: "primary.dark" }}
+                            className="AccordionDetails backdrop-saturate-50"
+                        >
+                            {" "}
+                            <div className=" h-[100%] w-[100%] fixed overflow-auto top-0 left-0 bg-black/60 -z-10"></div>
+
+                            <div className="z-40">{content[1]}</div>
+
                         </AccordionDetails>
                     </Accordion>
                 </div>
