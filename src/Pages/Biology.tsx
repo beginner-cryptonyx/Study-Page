e0fc13c82be68c2e69734ba9b28403e729816535
Crypto <mayanksethi242008@gmail.com>
2023-10-08 17:01:48 +0530

    Added whiteboard from miro to the biology concept map page

--- src/Pages/Biology.tsx
+++ src/Pages/Biology.tsx
@@ -1,5 +1,20 @@
 function FormulaeBiology() {
-    return <h1>yes</h1>
+    return (
+        <>
+            <div className="flex h-[80vh] mx-auto p-0">
+                <iframe
+                    title="Concept Map"
+                    width={window.innerWidth - 50}
+                    src="https://miro.com/app/live-embed/uXjVNdZYg6M=/?moveToViewport=-1402,-1001,2639,1543&embedId=626315562917&embedAutoplay=true"
+                    // frameborder="0"
+                    scrolling="no"
+                    allow="fullscreen; clipboard-read; clipboard-write"
+                    allowFullScreen
+                    style={{ marginLeft: "auto", marginRight: "auto" }}
+                ></iframe>
+            </div>
+        </>
+    );
 }
 
 export default FormulaeBiology;
