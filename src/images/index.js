e87f3aa2b138a40abcc2862dc923034b480b30ed
Crypto <mayanksethi242008@gmail.com>
2023-10-08 20:13:59 +0530

    Shifted

--- src/images/index.js
+++ src/images/index.js
@@ -1,13 +1,15 @@
-const bulb = require("../images/bulb.png");
-const ammeter = require("../images/ammeter.png");
-const cell_group = require("../images/cellgrp.png");
-const cell = require("../images/cell.png");
-const voltmeter = require("../images/voltmeter.png");
-const LDR = require("../images/LDR.png");
-const thermistor = require("../images/thermistor.png");
-const resistor = require("../images/resistor.png");
-const variable_resistor = require("../images/variable resistor.png");
-const fuse = require("../images/fuse.png");
+const bulb = require("../images/Electrical Symbols/bulb.png");
+const ammeter = require("../images/Electrical Symbols/ammeter.png");
+const cell_group = require("../images/Electrical Symbols/cellgrp.png");
+const cell = require("../images/Electrical Symbols/cell.png");
+const voltmeter = require("../images/Electrical Symbols/voltmeter.png");
+const LDR = require("../images/Electrical Symbols/LDR.png");
+const thermistor = require("../images/Electrical Symbols/thermistor.png");
+const resistor = require("../images/Electrical Symbols/resistor.png");
+const variable_resistor = require("../images/Electrical Symbols/variable resistor.png");
+const fuse = require("../images/Electrical Symbols/fuse.png");
+const LED = require("../images/Electrical Symbols/LED.png");
+const motor = require("../images/Electrical Symbols/motor.png");
 
 module.exports = {
     bulb: bulb,
@@ -20,4 +22,6 @@ module.exports = {
     resistor: resistor,
     variable_resistor: variable_resistor,
     fuse: fuse,
+    LED: LED,
+    motor: motor,
 };
